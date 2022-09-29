95ca0c3b1016515d0ccfbbc45e9917dba1885e23
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2022-09-29 11:40:04 -0300

    Update index.ts

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -1,27 +1,52 @@
 import { View, Text, TouchableOpacity } from "react-native";
-import { PencilSimple, TrashSimple } from "phosphor-react-native";
+import { PencilSimple, TrashSimple, Check } from "phosphor-react-native";
 
 import { styles } from "./styles";
+import { TaskObj } from "../../screens/Home";
 
-interface TaskProps {
-  id?: string;
-  title: string;
-  completed?: boolean;
+export interface TaskProps {
+  task: TaskObj;
+  handleToggleComplete: (task: TaskObj) => void;
+  handleDeleteTask: (id: string) => void;
 }
 
-export function Task({ id, title, completed }: TaskProps) {
+export function Task({
+  task,
+  handleToggleComplete,
+  handleDeleteTask,
+}: TaskProps) {
   return (
-    <View style={styles.container}>
-      <TouchableOpacity style={styles.btnCheck}></TouchableOpacity>
+    <View
+      style={[
+        styles.container,
+        task.completed ? styles.completedContainer : styles.container,
+      ]}
+    >
+      <TouchableOpacity
+        style={[
+          styles.btnCheck,
+          task.completed ? styles.btnCompleted : styles.btnCheck,
+        ]}
+        onPress={() => handleToggleComplete(task)}
+      >
+        {task.completed ? <Check size={14} weight="bold" color="#fff" /> : ""}
+      </TouchableOpacity>
 
-      <Text style={styles.title}>{title}</Text>
+      <Text
+        style={[
+          styles.description,
+          task.completed ? styles.completedDescription : styles.description,
+        ]}
+      >
+        {task.description}
+      </Text>
 
       <View style={styles.btnsContainer}>
         <TouchableOpacity style={styles.btnEdit}>
           <PencilSimple size={20} color="#f7f3f3" />
         </TouchableOpacity>
 
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => handleDeleteTask(task.id)}>
           <TrashSimple size={20} color="#f1524a" />
         </TouchableOpacity>
       </View>
