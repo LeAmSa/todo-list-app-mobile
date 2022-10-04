09de59e7e06403dec1818a936ac5a0f29c8dfc84
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2022-10-03 22:10:37 -0300

    Add own theme provider

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { StyleSheet, Text, View, SafeAreaView, StatusBar } from "react-native";
 import { Routes } from "./src/routes";
+import { darkTheme, lightTheme } from "./src/themes/theme";
+import { ThemeProvider } from "./src/themes/Theme";
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider>
       <StatusBar barStyle="default" backgroundColor="transparent" translucent />
       <Routes />
-    </>
+    </ThemeProvider>
   );
 }
